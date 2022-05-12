54e385b55ca7d1a6808c1be7ede30d17a29e4eaa
Catie Welsh <welshc@rhodes.edu>
2022-05-12 15:18:47 -0500

    Adding Lab 00

---
+++ labs/lab00/hello_world.py
@@ -0,0 +1,12 @@
+# Author:
+# Class:
+# Lab #: Lab 00
+# Pledge:
+# Description:
+
+# 1. FILL IN the required program heading above.
+# See the section on required comments on Canavs (Comment Guide).
+
+#2. Edit that program to print your name
+name = "COMP141 Student"
+print("Hello", name)
